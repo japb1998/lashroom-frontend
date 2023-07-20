14cc097a0e8f2a26f5e33b147f81731471dae1bc
Javier <japb1998@gmail.com>
2023-07-19 22:41:49 -0400

    adding pwa

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://938f97v570.execute-api.us-east-1.amazonaws.com/dev'
+  apiUrl: 'https://938f97v570.execute-api.us-east-1.amazonaws.com/dev',
+  debug: 'production'
 };
