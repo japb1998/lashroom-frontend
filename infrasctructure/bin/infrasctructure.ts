23bafbf6f6f5f836137a4e4d35df65d62cfaaed0
Javier <japb1998@gmail.com>
2023-07-24 23:27:24 -0400

    making a stable cicd

--- infrasctructure/bin/infrasctructure.ts
+++ infrasctructure/bin/infrasctructure.ts
@@ -4,6 +4,10 @@ import * as cdk from 'aws-cdk-lib';
 import { InfrasctructureStack } from '../lib/infrasctructure-stack';
 
 const app = new cdk.App();
-const stage = process.env.STAGE ?? 'dev'
-new InfrasctructureStack(app, `FrontEndStack-${stage}`, {
+const stage = process.env.STAGE ?? 'dev';
+
+if (!process.env.PROJECT) {
+    throw new Error('Project Name is required')
+}
+new InfrasctructureStack(app, `${process.env.PROJECT}-FrontEnd-${stage}`, {
 });
