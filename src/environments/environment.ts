6c90fcdbc102d79c26a2f06bbad3bbd52522f496
Javier <japb1998@gmail.com>
2023-07-24 20:55:17 -0400

    adding icons for all platforms

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   log:'debug',
   apiUrl: 'https://938f97v570.execute-api.us-east-1.amazonaws.com/dev',
-  // apiUrl: 'http://localhost:8080'
+  // apiUrl: 'http://localhost:3001'
 };
 
 /*
